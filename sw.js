3832b25a77933dc3202fc2bdf3d9d682a0e3dffe
170596 <86667248+170596@users.noreply.github.com>
2026-05-05 14:01:22 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'loc-ministries-v3';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './images_logo.jpeg',
+  './images_qr_code.jpeg',
+  'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
+];
+
+// Install: फाइलों को कैशे में सेव करना
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Activate: पुराने कैशे को हटाना (जब आप v1 से v2 करेंगे)
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            console.log('Service Worker: Clearing Old Cache');
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch: इंटरनेट न होने पर कैशे से फाइल देना
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
